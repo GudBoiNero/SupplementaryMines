391db7d2f00eafb93d7bc0e9e91954fa142bd6e3
GudBoiNero <donkeykong797@gmail.com>
2023-12-19 18:39:17 -0500

    boom

---
+++ SupplementaryMines/obj/Release/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace SupplementaryMines
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "SupplementaryMines";
+        public const string PLUGIN_NAME = "SupplementaryMines";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
