8be02bbf836b6e97c5d229fc5bb8da118491b959
GudBoiNero <donkeykong797@gmail.com>
2023-12-19 17:29:28 -0500

    important flavor text :>

--- SupplementaryMines/obj/Debug/netstandard2.1/SupplementaryMines.AssemblyInfo.cs
+++ SupplementaryMines/obj/Debug/netstandard2.1/SupplementaryMines.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupplementaryMines")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("My first plugin")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Lethal Company requires more explosives. Don\'t you agree?")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupplementaryMines")]
