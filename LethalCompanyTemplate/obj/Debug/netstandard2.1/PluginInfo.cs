bb732053f16b71312d556317b2a4d7f6c7c0e558
GudBoiNero <donkeykong797@gmail.com>
2023-12-19 17:20:52 -0500

    config

--- LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
+++ LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -2,8 +2,8 @@ namespace SupplementaryMines
 {
     public static class PluginInfo
     {
-        public const string PLUGIN_GUID = "LethalCompanyTemplate";
-        public const string PLUGIN_NAME = "LethalCompanyTemplate";
+        public const string PLUGIN_GUID = "SupplemtaryMines_GudBoiNero";
+        public const string PLUGIN_NAME = "SupplemtaryMines";
         public const string PLUGIN_VERSION = "1.0.0";
     }
 }
