397e430ff674b6451468ef29ca3fc88ff8d83f81
GudBoiNero <donkeykong797@gmail.com>
2023-12-19 22:00:23 -0500

    this is frustrating
    Having a lot of difficulty adding `Assembly-CSharp.dll` into the `.csproj` file as a reference. Otherwise I can't interface with the game.

--- SupplementaryMines/Plugin.cs
+++ SupplementaryMines/Plugin.cs
@@ -1,14 +1,22 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngineInternal;
 
 namespace SupplementaryMines
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
         private void Awake()
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            harmony.PatchAll(typeof(Plugin));
         }
     }
 }
