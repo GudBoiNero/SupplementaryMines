a4056e0037542ef295a633ae56d6611fcddda350
GudBoiNero <donkeykong797@gmail.com>
2023-12-20 19:37:57 -0500

    got the patch to work

--- SupplementaryMines/Plugin.cs
+++ SupplementaryMines/Plugin.cs
@@ -1,19 +1,30 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
 using HarmonyLib;
+using SupplementaryMines.Patches;
+using UnityEngine;
 
 namespace SupplementaryMines
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        public static Plugin instance = null;
         Harmony harmony = new(PluginInfo.PLUGIN_GUID);
 
         private void Awake()
         {
+            instance = this;
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
 
             harmony.PatchAll(typeof(Plugin));
+            harmony.PatchAll(typeof(RoundManagerPatch));
+        }
+
+        public static void Log(string message)
+        {
+            instance.Logger.LogInfo((object)message);
         }
     }
 }
---
+++ SupplementaryMines/patches/RoundManagerPatch.cs
@@ -0,0 +1,28 @@
+using BepInEx;
+using HarmonyLib;
+using SupplementaryMines.Patches;
+using DunGen;
+
+namespace SupplementaryMines.Patches
+{
+    [HarmonyPatch(typeof(RoundManager))]
+    internal class RoundManagerPatch
+    {
+        public static RoundManagerPatch Instance = null;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+        }
+
+        [HarmonyPatch("SpawnMapObjects")]
+        [HarmonyPostfix]
+        static void SpawnMapObjects()
+        {
+            Plugin.Log("Called SpawnMapObjects");
+        }
+    }
+}
