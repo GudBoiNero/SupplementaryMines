4804c6f3bbb9d696f0157e685e4f0352668769e1
GudBoiNero <donkeykong797@gmail.com>
2023-12-23 17:37:44 -0500

    adding config

--- SupplementaryMines/config/Config.cs
+++ SupplementaryMines/config/Config.cs
@@ -15,6 +15,10 @@ namespace SupplementaryMines.config
         private static ConfigEntry<bool> _consistentLandmines;
         private static ConfigEntry<float> _landminesMultiplier;
         private static ConfigEntry<float> _landminesMinimum;
+        private static ConfigEntry<bool> _hellmines;
+        private static ConfigEntry<float> _hellminesDetectionRange;
+
+
 
         public static void Init()
         {
@@ -23,10 +27,12 @@ namespace SupplementaryMines.config
             var filePath = Path.Combine(Paths.ConfigPath, CONFIG_FILE_NAME);
             _config = new ConfigFile(filePath, true);
 
-            _consistentLandmines = _config.Bind("Config", "Consistent Landmines", false, "Every map will have mines in their spawn table.");
-            _landminesMultiplier = _config.Bind("Config", "Landmines Multiplier", 1.0f, "Multiplies the default amount of mines to spawn.");
-            _landminesMinimum = _config.Bind("Config", "Landmines Minimum", 1.0f, "[Only applies with Consistent Landmines turned on] The minimum amount of mines to spawn.");
-            
+            _consistentLandmines = _config.Bind("Config", "Consistent Landmines", false, "Every map will have landmines in their spawn table.");
+            _landminesMultiplier = _config.Bind("Config", "Landmines Multiplier", 1.0f, "Multiplies the default amount of landmines to spawn.");
+            _landminesMinimum = _config.Bind("Config", "Landmines Minimum", 1.0f, "[Only applies with Consistent Landmines enabled] The minimum amount of landmines to spawn.");
+            _hellmines = _config.Bind("Config", "Hellmines", false, "Landmines are now bigger and can detect you without being stepped on...");
+            _hellminesDetectionRange = _config.Bind("Config", "Hellmines", 0f, "[Only applies with Hellmines enabled] The hellmines' detection range.");
+
             Plugin.Log("Config initialized!");
         }
 
@@ -35,10 +41,14 @@ namespace SupplementaryMines.config
             Plugin.Log($"Consistent Landmines: {consistentLandmines}");
             Plugin.Log($"Landmines Multiplier: {landminesMultiplier}");
             Plugin.Log($"Landmines Multiplier: {landminesMinimum}");
+            Plugin.Log($"Hellmines: {hellmines}");
+            Plugin.Log($"Hellmines Detection Range: {hellminesDetectionRange}");
         }
 
         public static bool consistentLandmines => _consistentLandmines.Value;
         public static float landminesMultiplier => _landminesMultiplier.Value;
         public static float landminesMinimum => _landminesMinimum.Value;
+        public static bool hellmines => _hellmines.Value;
+        public static float hellminesDetectionRange => _hellminesDetectionRange.Value;
     }
 }
